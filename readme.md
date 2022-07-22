3c11e3856c4a75ab96b6963cd91320c367709a94
Chandresh98 <99880909+Chandresh98@users.noreply.github.com>
2022-07-22 18:46:21 +0530

    3 api done

--- readme.md
+++ readme.md
@@ -1,9 +1,32 @@
+1 api post
+
 /*fetching N news articles*/
 
 localhost:3000/newsArticles/:number 
 where number is the numbers of news Articles you have to see it is between 1-10 because of free account so it is handle in code to view only 1-10 articles at a time
 
 for Example: localhost:3000/newsArticles/1
+
+2 get api
+
+/*finding a news article with a specific title or author*/
+localhost:3000/newsArticles
+
+author and title with same name in quary params 
+for example:{
+    author:TechCrunch
+    title:m   // you can give substring
+}
+
+atleast 1 is required
+
+
+3 get api
+/* searching by keywords*/
+keyword with same name in quary params like news, tecnology etc
+
+
+
 
 
 
--- route.js
+++ route.js
@@ -13,7 +13,7 @@ router.post('/newsArticles/:number',async (req, res) => {
         if(no>10 || no<1){
             return res.status(400).send({status:false,message:"please enter between 1-10 articles at a time"})
         }
-       const abc= await axios.get(`https://gnews.io/api/v4/search?q=example&token=4f3895259bb0c0139f79bc4ef540ce2b&max=${no}`)
+       const abc= await axios.get(`https://gnews.io/api/v4/search?q=example&token=fb69ac0cee34110cbbaf641878743d30&max=${no}`)
        let data = abc.data.articles
        res.status(200).send({status:true,message:"successful",totalArticles:no,data:data})
   
@@ -25,9 +25,61 @@ router.post('/newsArticles/:number',async (req, res) => {
 })
 
 
-router.get('/')
+router.get('/newArticles' , async (req,res)=>{
+  try{  const title = req.query.title 
+    const author = req.query.author
+    const abc= await axios.get(`https://gnews.io/api/v4/search?q=example&token=fb69ac0cee34110cbbaf641878743d30`)
+    const lengthOfArtices = abc.data.articles
+
+    if(!title && !author){
+        return res.status(400).send({status:false,message:"please enter alteast one entity author or title"})
+    }
+    
+    
+   if(author){ for(let i=0;i<lengthOfArtices.length;i++){
+        let nameOfAuthor = lengthOfArtices[i].source.name.toLowerCase()
+        if(author.toLowerCase() == nameOfAuthor){
+            return res.status(200).send({
+              status:true,
+              data:lengthOfArtices[i]
+            })
+        }
+    }
+    return res.status(404).send({status:false,message:"invalid author"})
+}
+    if(title){
+        for(let i=0;i<lengthOfArtices.length;i++){
+            let titleInSmall = title.toLowerCase()
+            let substringOftitle = lengthOfArtices[i].title.substring(0,title.length)
+           let titleInArticle= substringOftitle.toLowerCase()
+           if(titleInSmall == titleInArticle){
+            return res.status(200).send({status:true,message:"successful", data:lengthOfArtices[i]})
+           }
+        }
+        return res.status(404).send({status:false,message:"invalid title"})
+    }
+}catch(error){
+    return res.status(404).send({status:false,message:error.message})
+}
+})
+
+
+router.get('/searchByKeyword', async (req,res)=>{
+    try{
+        const keyword = req.query.keyword
+        if(!keyword){
+            return res.status(400).send({status:false,message:"please enter keyword"})
+        }
+        const abc= await axios.get(`https://gnews.io/api/v4/search?q=${keyword}&token=fb69ac0cee34110cbbaf641878743d30`)
+         let data = abc.data.articles
+         return res.status(200).send({status:true,message:"successful",totalArticles:data.length ,data:data})
+         
+    }catch(error){
+        return res.status(404).send({status:false,message:error.message})
+    }
+})
 
 
 
 
-modu
+module.exports = router
